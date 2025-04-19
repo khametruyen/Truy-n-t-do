f2ffdfc26bee968d5345321f58628e3a55b874ab
khametruyen <tandekhoinguyen@gmail.com>
2025-04-19 19:42:22 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Web đọc truyện Tự Do</h1>
-      <p>Chào mừng bạn đến với trang chủ.</p>
-    </div>
-  );
-}
