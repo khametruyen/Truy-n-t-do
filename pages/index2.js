6fbf903ed0ad30f1a14a68cfa0306473619cf2a2
khametruyen <tandekhoinguyen@gmail.com>
2025-04-19 19:08:35 +0700

    Create index2.js

---
+++ pages/index2.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Web đọc truyện Tự Do</h1>
+      <p>Chào mừng bạn đến với trang chủ!</p>
+    </div>
+  );
+}
+Tạo file index.js trong thư mục pages
